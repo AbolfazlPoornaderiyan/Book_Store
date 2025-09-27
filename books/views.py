36ea31912da313a63a9f37ff4dd5714624a42f13
Abolfazl Poornaderiyan <abolfazlpoor1461@gmail.com>
2025-09-27 23:50:57 +0330

    add permission in views.

--- books/views.py
+++ books/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from .models import Book
 from .serializers import BookSerializer
@@ -8,3 +9,4 @@ class BookViewSet(viewsets.ModelViewSet):
 	queryset = Book.objects.all()
 	serializer_class = BookSerializer
 
+	permission_classes = [IsAuthenticatedOrReadOnly]
